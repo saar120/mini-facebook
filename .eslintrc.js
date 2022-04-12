f432f285c89b1b27409e1c1cf1554b761885d8eb
Saar Amrani <saar120@gmail.com>
2022-04-12 17:30:57 +0300

    added linter

---
+++ .eslintrc.js
@@ -0,0 +1,37 @@
+const error = 2;
+const warn = 1;
+const ignore = 0;
+
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: ["plugin:react/recommended", "airbnb", "prettier"],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react", "prettier"],
+  rules: {
+    "prettier/prettier": [
+      warn,
+      {
+        printWidth: 100,
+        tabWidth: 2,
+        bracketSpacing: true,
+        trailingComma: "es5",
+        singleQuote: false,
+      },
+    ],
+    "react/jsx-filename-extension": [
+      warn,
+      {
+        extensions: [".js", ".jsx"],
+      },
+    ],
+  },
+};
